0ebd0c4a29d9bd400e979da3d811cb30312ad5c1
tracer-nju <tracer@nju.edu.cn>
2025-12-15 22:08:11 +0800

    Static hostname: LAPTOP-A082VO9J        Icon name: computer-container          Chassis: container ☐       Machine ID: d6f9fe49721a44188d97ee7b91d9d4be          Boot ID: 956d0dbf265b4a9696a921d09b8bc743   Virtualization: wsl Operating System: Ubuntu 24.04.3 LTS           Kernel: Linux 5.15.167.4-microsoft-standard-WSL2     Architecture: x86-64  22:08:11 up 18 min,  1 user,  load average: 0.42, 0.23, 0.12

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include <inttypes.h>
+#include <string.h>
 
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
@@ -8,17 +9,76 @@ static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
-// TODO: implement the following functions
+struct cache_line {
+  uint32_t data[BLOCK_SIZE / sizeof(uint32_t)];
+  uintptr_t tag;
+  bool valid;
+  bool dirty;
+} **cache;
+static int tag_start;
+static size_t group_lines;
 
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  uintptr_t tag = addr >> tag_start;
+  struct cache_line *empty = NULL,
+                    *group =
+                        cache[(addr & mask_with_len(tag_start)) >> BLOCK_WIDTH];
+  for (struct cache_line *line = group + group_lines; line-- > group;)
+    if (!line->valid)
+      empty = line;
+    else if (line->tag == tag)
+      return line->data[(addr & (BLOCK_SIZE - 1)) / sizeof(uint32_t)];
+  if (!empty) {
+    empty = group + (rand() & (group_lines - 1));
+    if (empty->dirty)
+      mem_write(empty->tag << (tag_start - BLOCK_WIDTH),
+                (uint8_t *)empty->data);
+  }
+  mem_read(addr >> BLOCK_WIDTH, (uint8_t *)empty->data);
+  empty->tag = tag;
+  empty->valid = true;
+  empty->dirty = false;
+  return empty->data[(addr & (BLOCK_SIZE - 1)) / sizeof(uint32_t)];
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  uintptr_t tag = addr >> tag_start;
+  struct cache_line *empty = NULL,
+                    *group =
+                        cache[(addr & mask_with_len(tag_start)) >> BLOCK_WIDTH];
+  for (struct cache_line *line = group + group_lines; line-- > group;)
+    if (!line->valid)
+      empty = line;
+    else if (line->tag == tag) {
+      uint32_t *p = &line->data[(addr & (BLOCK_SIZE - 1)) / sizeof(uint32_t)];
+      *p = (*p & ~wmask) | (data & wmask);
+      line->dirty = true;
+      return;
+    }
+  if (!empty) {
+    empty = group + (rand() & (group_lines - 1));
+    if (empty->dirty)
+      mem_write(empty->tag << (tag_start - BLOCK_WIDTH),
+                (uint8_t *)empty->data);
+  }
+  mem_read(addr >> BLOCK_WIDTH, (uint8_t *)empty->data);
+  empty->tag = tag;
+  empty->valid = true;
+  uint32_t *p = &empty->data[(addr & (BLOCK_SIZE - 1)) / sizeof(uint32_t)];
+  *p = (*p & ~wmask) | (data & wmask);
+  empty->dirty = true;
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  tag_start = total_size_width - associativity_width;
+  group_lines = (size_t)1 << associativity_width;
+  const size_t group_number = (size_t)1 << (tag_start - BLOCK_WIDTH);
+  cache = malloc(group_number * sizeof(struct cache_line *));
+  for (struct cache_line **group = cache + group_number; group-- > cache;) {
+    *group = malloc(group_lines * sizeof(struct cache_line));
+    for (struct cache_line *line = *group + group_lines; line-- > *group;)
+      line->valid = false;
+  }
 }
 
-void display_statistic(void) {
-}
+void display_statistic(void) {}
